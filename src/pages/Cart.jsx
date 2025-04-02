df7bef2ff002b2ecf5f6d57e515983fba94ee630
OscarGarcia <discoveroscardo@gmail.com>
2025-04-02 20:04:41 +0200

    desarrollando frontend

---
+++ src/pages/Cart.jsx
@@ -0,0 +1,11 @@
+const Cart = () => {
+    return (
+      <div>
+        <h1 className="text-2xl font-bold">Carrito de Compras</h1>
+        <p className="text-gray-600">Aquí aparecerán los productos que añadas al carrito.</p>
+      </div>
+    );
+  };
+  
+  export default Cart;
+  
